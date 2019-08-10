494736f95680da9f40394b50c18895ccb2e70ca6
Bojo <bozhidar_zahariev@hotmail.com>
2019-08-10 12:11:43 +0100

    some improvements

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-'use strict';
 //Nixies
-var but1L = document.getElementById('but1L');
-var but2L = document.getElementById('but2L');
-var but3L = document.getElementById('but3L');
-var but4L = document.getElementById('but4L');
-var but5L = document.getElementById('but5L');
-var but6L = document.getElementById('but6L');
-var but7L = document.getElementById('but7L');
-var but8L = document.getElementById('but8L');
-var but9L = document.getElementById('but9L');
-var but0L = document.getElementById('but0L');
-var butAL = document.getElementById('butAL');
-var butMuL = document.getElementById('butMuL');
-var butSL = document.getElementById('butSL');
-var butCL = document.getElementById('butCL');
-var butBkL = document.getElementById('butBkL');
-var butDivL = document.getElementById('butDivL');
-var butDecL = document.getElementById('butDecL');
-var butEL = document.getElementById('butEL');
 
 var dis = document.getElementById('dis');
 var list = document.getElementById('list');
@@ -30,225 +11,73 @@ var res;
 var a = '';
 
 //Keyboard support
-document.addEventListener('keydown', function(event) {
-
+document.addEventListener('keydown', function (event) {
   if (event.which === 97 || event.which === 49) {
-    if (op === '') {
-      num1 += '1';
-      a += '1';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '1';
-      a += '1';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-    //Nixie light
-    but1L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but1L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(1);
   }
 }, false);
 
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 98 || event.which === 50) {
-    if (op === '') {
-      num1 += '2';
-      a += '2';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '2';
-      a += '2';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-    but2L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but2L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(2);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 99 || event.which === 51) {
-    if (op === '') {
-      num1 += '3';
-      a += '3';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '3';
-      a += '3';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-    but3L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but3L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(3);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 100 || event.which === 52) {
-    if (op === '') {
-      num1 += '4';
-      a += '4';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '4';
-      a += '4';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-    but4L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but4L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(4);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 101 || event.which === 53) {
-    if (op === '') {
-      num1 += '5';
-      a += '5';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '5';
-      a += '5';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-    but5L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but5L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(5);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 102 || event.which === 54) {
-    if (op === '') {
-      num1 += '6';
-      a += '6';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '6';
-      a += '6';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-
-    but6L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but6L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(6);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 103 || event.which === 55) {
-    if (op === '') {
-      num1 += '7';
-      a += '7';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '7';
-      a += '7';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-
-    but7L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but7L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(7);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 104 || event.which === 56) {
-    if (op === '') {
-      num1 += '8';
-      a += '8';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '8';
-      a += '8';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-
-    but8L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but8L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(8);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 105 || event.which === 57) {
-    if (op === '') {
-      num1 += '9';
-      a += '9';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '9';
-      a += '9';
-      list.textContent = a;
-      dis.textContent = num2;
-    }
-
-    but9L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but9L.style.filter = "grayscale(100%)";
-      }, 1000);
+    clickNumber(9);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 96 || event.which === 48) {
-    if (op === '') {
-      num1 += '0';
-      a += '0';
-      list.textContent = a;
-      dis.textContent = num1;
-    } else if (op !== '') {
-      num2 += '0';
-      a += '0';
-      list.textContent = a;
-      dis.textContent = num2;
+    if (op === 'div') {
+      reset();
     }
 
-    but0L.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        but0L.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(10);
+    clickNumber(0);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 107 || event.which === 187) {
     if (op !== '' & num2 !== '') {
       operate();
@@ -259,15 +88,11 @@ document.addEventListener('keydown', function(event) {
     }
     op = 'add';
 
-    butAL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butAL.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(12);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 109 || event.which === 189) {
     if (op !== '' & num2 !== '') {
       operate();
@@ -283,15 +108,11 @@ document.addEventListener('keydown', function(event) {
       list.textContent = a;
     }
 
-    butSL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butSL.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(13);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 106) {
     if (op !== '' & num2 !== '') {
       operate();
@@ -302,15 +123,11 @@ document.addEventListener('keydown', function(event) {
     }
     op = 'mult';
 
-    butMuL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butMuL.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(15);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 111) {
     if (op !== '' & num2 !== '') {
       operate();
@@ -320,15 +137,11 @@ document.addEventListener('keydown', function(event) {
       list.textContent = a;
     }
     op = 'div';
-    butDivL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butDivL.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(16);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 110 || event.which === 190) {
     if (num1.toString().indexOf('.') === -1 & op === '') {
       num1 += '.';
@@ -341,16 +154,13 @@ document.addEventListener('keydown', function(event) {
       a += '.';
       list.textContent = a;
     }
-    butDecL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butDecL.style.filter = "grayscale(100%)";
-      }, 1000);
+
+    lightEmUp(17);
   }
 }, false);
 
 //Backspace
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 8) {
     if (dis.textContent == num1 & op === '') {
       num1 = num1.toString().slice(0, -1);
@@ -363,244 +173,67 @@ document.addEventListener('keydown', function(event) {
     }
     a = a.toString().slice(0, -1);
     list.textContent = a;
-    butBkL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butBkL.style.filter = "grayscale(100%)";
-      }, 1000);
+
+    lightEmUp(14);
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 13) {
 
     operate();
-    butEL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butEL.style.filter = "grayscale(100%)";
-      }, 1000);
+    lightEmUp(18);
 
   }
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.which === 27) {
     reset();
-    butCL.style.filter = "grayscale(0%)";
-    setTimeout(
-      function() {
-        butCL.style.filter = "grayscale(100%)";
-      }, 1000);
-
+    lightEmUp(11);
   }
 }, false);
 
 //Number buttons
-var btn1 = document.getElementById('btn1');
-btn1.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '1';
-    a += '1';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '1';
-    a += '1';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but1L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but1L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
 
-var btn2 = document.getElementById('btn2');
-btn2.addEventListener('click', () => {
+//Display numbers
+function clickNumber(n) {
   if (op === '') {
-    num1 += '2';
-    a += '2';
+    num1 += n;
+    a += n;
     list.textContent = a;
     dis.textContent = num1;
   } else if (op !== '') {
-    num2 += '2';
-    a += '2';
+    num2 += n;
+    a += n;
     list.textContent = a;
     dis.textContent = num2;
   }
-  but2L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but2L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
 
-var btn3 = document.getElementById('btn3');
-btn3.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '3';
-    a += '3';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '3';
-    a += '3';
-    list.textContent = a;
-    dis.textContent = num2;
+  if (n !== 0) {
+    lightEmUp(n);
   }
-  but3L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but3L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
+}
 
-var btn4 = document.getElementById('btn4');
-btn4.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '4';
-    a += '4';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '4';
-    a += '4';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but4L.style.filter = "grayscale(0%)";
+//Light
+const lightEmUp = (light) => {
+  var child = document.querySelectorAll('.light');
+  child[light - 1].style.filter = "grayscale(0%)";
   setTimeout(
-    function() {
-      but4L.style.filter = "grayscale(100%)";
+    function () {
+      child[light - 1].style.filter = "grayscale(100%)";
     }, 1000);
-}, false);
+}
 
-var btn5 = document.getElementById('btn5');
-btn5.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '5';
-    a += '5';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '5';
-    a += '5';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but5L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but5L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
-
-var btn6 = document.getElementById('btn6');
-btn6.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '6';
-    a += '6';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '6';
-    a += '6';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but6L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but6L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
-
-var btn7 = document.getElementById('btn7');
-btn7.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '7';
-    a += '7';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '7';
-    a += '7';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but7L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but7L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
-
-var btn8 = document.getElementById('btn8');
-btn8.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '8';
-    a += '8';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '8';
-    a += '8';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but8L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but8L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
-
-var btn9 = document.getElementById('btn9');
-btn9.addEventListener('click', () => {
-  if (op === '') {
-    num1 += '9';
-    a += '9';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '9';
-    a += '9';
-    list.textContent = a;
-    dis.textContent = num2;
-  }
-  but9L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but9L.style.filter = "grayscale(100%)";
-    }, 1000);
-}, false);
 
 var btn0 = document.getElementById('btn0');
 btn0.addEventListener('click', () => {
-  if (op === 'div' & num2 === '') {
-    dis.textContent = 'Hey don\'t do that';
-    setTimeout(
-      function() {
-        reset();
-      }, 1000);
-  } else if (op === '') {
-    num1 += '0';
-    a += '0';
-    list.textContent = a;
-    dis.textContent = num1;
-  } else if (op !== '') {
-    num2 += '0';
-    a += '0';
-    list.textContent = a;
-    dis.textContent = num2;
+  if (op === 'div') {
+    reset();
   }
-  but0L.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      but0L.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(10);
+  clickNumber(0);
+
 }, false);
 
 
@@ -617,14 +250,10 @@ btnA.addEventListener('click', () => {
     list.textContent = a;
   }
   op = 'add';
-  butAL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butAL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(12);
 }, false);
 
-//Substraction and minus button
+//Subtraction and minus button
 var btnS = document.getElementById('btnS');
 btnS.addEventListener('click', () => {
   //Operate if operator is pressed instead of '='
@@ -642,11 +271,7 @@ btnS.addEventListener('click', () => {
     a += '-';
     list.textContent = a;
   }
-  butSL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butSL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(13);
 }, false);
 
 //Multiplication button
@@ -661,11 +286,7 @@ btnMu.addEventListener('click', () => {
     list.textContent = a;
   }
   op = 'mult';
-  butMuL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butMuL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(15);
 }, false);
 
 //Division button
@@ -680,11 +301,7 @@ btnDiv.addEventListener('click', () => {
     list.textContent = a;
   }
   op = 'div';
-  butDivL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butDivL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(16);
 }, false);
 
 
@@ -692,14 +309,8 @@ btnDiv.addEventListener('click', () => {
 //Operate button
 var btnE = document.getElementById('btnE');
 btnE.addEventListener('click', () => {
-
+  lightEmUp(18);
   operate();
-  butEL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butEL.style.filter = "grayscale(100%)";
-    }, 1000);
-
 }, false);
 
 //Decimal button
@@ -717,22 +328,14 @@ btnDec.addEventListener('click', () => {
     a += '.';
     list.textContent = a;
   }
-  butDecL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butDecL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(17);
 }, false);
 
 //Reset button
 var btnC = document.getElementById('btnC');
 btnC.addEventListener('click', () => {
   reset();
-  butCL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butCL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(11);
 
 }, false);
 
@@ -750,11 +353,7 @@ btnBk.addEventListener('click', () => {
   }
   a = a.toString().slice(0, -1);
   list.textContent = a;
-  butBkL.style.filter = "grayscale(0%)";
-  setTimeout(
-    function() {
-      butBkL.style.filter = "grayscale(100%)";
-    }, 1000);
+  lightEmUp(14);
 }, false);
 
 //Reset function
@@ -773,7 +372,7 @@ function add(num1, num2) {
   return res;
 }
 
-//Substraction function
+//Subtraction function
 function substract(num1, num2) {
   res = Number(num1) - Number(num2);
   return res;
